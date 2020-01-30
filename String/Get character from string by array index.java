0f4e4ee97954946527aa8182761b46b1a577720a
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-01-30 14:40:12 +0600

    Get character from string by array index java code

---
+++ String/Get character from string by array index.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class GetStringVale {
+
+    public static void main(String[] args) {
+       
+       String s="sabbir";
+       System.out.printf("%c\n",s.charAt(4));
+    }
+    
+}
+
+// will print i which is in 4th index of string s.
