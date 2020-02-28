a0c7a4650e5f967d4ae7ca1f69119ae1ffa98e92
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-02-29 03:20:23 +0600

    Update Copy an Array.java

--- Array/Copy an Array.java
+++ Array/Copy an Array.java
@@ -1,12 +1,20 @@
 public class ArrayCopy {
+    
+    /*
+	
+	Sabbir Ahmed,Comilla University
+	GitHub> https://github.com/ArafatSabbir
+	
+	*/
 
     public static void main(String[] args) {
        int arr[]={9,8,7,6,5,4,3,2,1,0};
        int arr2[]=new int[10];
        
-       System.arraycopy(arr, 3, arr2, 2, 5); // arr is 1st array,3 is index of arr, arr2 2nd arry, 2 index of arr2, 5 numbers of elements. 
-       for(int a:arr2){
-       System.out.print(" "+a);
+       System.arraycopy(arr, 3, arr2, 2, 5);   // arr is 1st array,3 is index of arr, arr2 2nd arry, 2 index of arr2, 5 numbers of elements. 
+       for(int a:arr2)                         // for each loop to print values of 2nd array.
+       {
+       System.out.print(" "+ a);
        }
     }
 }
