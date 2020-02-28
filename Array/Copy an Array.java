57e8d7e01c89ebb4d3d62109a863d109ca93aca4
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-02-29 03:16:44 +0600

    Copy an Array to 2nd array.

---
+++ Array/Copy an Array.java
@@ -0,0 +1,14 @@
+public class ArrayCopy {
+
+    public static void main(String[] args) {
+       int arr[]={9,8,7,6,5,4,3,2,1,0};
+       int arr2[]=new int[10];
+       
+       System.arraycopy(arr, 3, arr2, 2, 5); // arr is 1st array,3 is index of arr, arr2 2nd arry, 2 index of arr2, 5 numbers of elements. 
+       for(int a:arr2){
+       System.out.print(" "+a);
+       }
+    }
+}
+
+// output is :  0 0 6 5 4 3 2 0 0
